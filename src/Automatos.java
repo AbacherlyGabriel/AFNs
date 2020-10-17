beca5fd2756ef1068ebe990e7234e143fc6ae1c3
AbacherlyGabriel <gaabacherly@usp.br>
2020-10-17 13:23:52 -0300

    Testando e Ajustando Detalhes Finais de Entrega

--- src/Automatos.java
+++ src/Automatos.java
@@ -155,6 +155,31 @@ public class Automatos {
     }
 
     /*
+    Verificando todos os estados alcancaveis por transicao de cadeia vazia
+    apos a leitura da cadeia ser finalizada
+
+    Parametros:
+
+    estado: Estado que sera analisado
+    indexEstado: Estado atual do automato
+     */
+
+    private boolean estadosAlcancaveis(List<Estado> estado, int indexEstado) {
+        if (this.estadosDeAceitacao.contains(indexEstado)) {
+            return true;
+        }
+
+        for (int i = 0; i < estado.size(); i++) {
+            if (estado.get(i).getSimbolo() == 0) {
+                List<Estado> estadoDestino = this.afn.get(estado.get(i).getDestino());
+                return estadosAlcancaveis(estadoDestino, estado.get(i).getDestino());
+            }
+        }
+
+        return false;
+    }
+
+    /*
     Leitura recursiva das cadeias de teste
     Para as cadeias aceitas, retorna-se true
 
@@ -179,20 +204,10 @@ public class Automatos {
         // Condicao de parada: cadeia finalizada
 
         if ((indexSimbolo + 1) > cadeia.size()) {
-            List<Integer> estadosAlcancaveis = new ArrayList<>();           // Instanciando lista de alcancaveis por cadeia vazia
-
-            // Preenchendo lista com estados alcancaveis por meio de cadeia vazia
-
-            for (int i = 0; i < estado.size(); i++) {
-                if (estado.get(i).getSimbolo() == 0) {
-                    estadosAlcancaveis.add(estado.get(i).getDestino());
-                }
-            }
-
             // Checando se o estado em analise ou algum dos alcancaveis esta entre os estados de aceitacao
 
             for (int estadoDeAceitacao : this.estadosDeAceitacao) {
-                if ((estadoDeAceitacao == indexEstado) || (estadosAlcancaveis.contains(estadoDeAceitacao))) {
+                if ((estadoDeAceitacao == indexEstado) || (estadosAlcancaveis(estado, indexEstado))) {
                     return true;
                 }
             }
